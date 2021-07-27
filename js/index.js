a8bf4c02fa7aaec4a1aae843f7a4fb9fb1b861ef
Alexandre Costa <alexandre.a.costa@inesctec.pt>
2021-07-27 17:56:56 +0100

    First import;

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var urls = {
+    "adc":        "",
+    "middleware": "",
+    "dashboard":  "",
+    "auth":       ""
+};
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+var clickables = document.getElementsByClassName("entity");
+
+for (var c of clickables) {
+    if (c.id && urls[c.id]) {
+        var url = urls[c.id];
+
+        c.addEventListener("click", function() {
+            window.open(urls[this.id], '_blank');
+        });
+    }
+}
